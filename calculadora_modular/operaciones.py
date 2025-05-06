880a357d08b47cc5ec9fe226d56caa1490554392
Anyi <anyicamilamendivelsorojas@gmail.com>
2025-05-06 08:23:37 -1000

    Primer commit: agrega main.py y operaciones.py

--- calculadora_modular/operaciones.py
+++ calculadora_modular/operaciones.py
@@ -6,18 +6,3 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
-def multiplicar(a, b):
-    return a * b
-
-def dividir(a, b):
-    if b == 0:
-        return "Error: división por cero"
-    return a / b
-
-def potencia(a, b):
-    return a ** b
-
-def division_entera(a, b):
-    if b == 0:
-        return "Error: división por cero"
-    return a // b
